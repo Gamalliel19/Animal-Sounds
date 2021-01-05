891d580db6f411a25b56c596528e4bc438a7026e
Gamalliel Sharon <you@example.com>
2021-01-05 19:26:56 +0700

    first commit

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print(sender.currentTitle!)
+        playSound(soundTitle: sender.currentTitle!)
+    }
+    
+    func playSound(soundTitle: String) {
+        let url = Bundle.main.url(forResource: soundTitle, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
 
